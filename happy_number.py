d14767044169fbcfc746105a52f0ff4d2f698d2a
Praveen240503 <96769580+Praveen240503@users.noreply.github.com>
2022-04-24 21:35:11 +0530

    import math
    def digitsSquareSum(Number):
        Sum = rem = 0
        while Number > 0:
            rem = Number % 10
            Sum = Sum + math.pow(rem, 2)
            Number = Number // 10
        return Sum
    
    
    Number = int(input())
    Temp = Number
    
    while Temp != 1 and Temp != 4:
        Temp = digitsSquareSum(Temp)
    
    if Temp == 1:
        print("True")
    else:
        print("False")

---
+++ happy_number.py
@@ -0,0 +1,21 @@
+import math
+
+def digitsSquareSum(Number):
+    Sum = rem = 0
+    while Number > 0:
+        rem = Number % 10
+        Sum = Sum + math.pow(rem, 2)
+        Number = Number // 10
+    return Sum
+
+
+Number = int(input())
+Temp = Number
+
+while Temp != 1 and Temp != 4:
+    Temp = digitsSquareSum(Temp)
+
+if Temp == 1:
+    print("True")
+else:
+    print("False")
