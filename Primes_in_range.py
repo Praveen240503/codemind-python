33a74d736e044c1395532a834c579dc09b6371df
Praveen240503 <96769580+Praveen240503@users.noreply.github.com>
2022-05-21 17:25:04 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     return 1 a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if prime(i):         c+=1 if c==0:     print('-1') else:     print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,8 +1,8 @@
-def prime(a):
-    if a==1:
+def prime(n):
+    if n==1:
         return 0
-    for i in range(2,int(a**0.5)+1):
-        if a%i==0:
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
             return 0
     return 1
 a=int(input())
