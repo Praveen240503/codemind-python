ac7430515a85f8980eff7516f7ed99b0a341c7e4
Praveen240503 <96769580+Praveen240503@users.noreply.github.com>
2022-05-25 23:49:48 +0530

    def dig(n):     v=0     while n!=0:         x=n%10         v+=1         n//=10     return v n=int(input()) a=list(map(int,input().split())) x=0 e=0 c=[] for i in a:     x=dig(i)     c.append(x) d=min(c) for j in c:     if d==j:         e+=1 print(e)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,20 @@
+def dig(n):
+    v=0
+    while n!=0:
+        x=n%10
+        v+=1
+        n//=10
+    return v
+n=int(input())
+a=list(map(int,input().split()))
+x=0
+e=0
+c=[]
+for i in a:
+    x=dig(i)
+    c.append(x)
+d=min(c)
+for j in c:
+    if d==j:
+        e+=1
+print(e)
