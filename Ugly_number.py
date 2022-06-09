2ad2f8f071f310c818fab733633c1a6c5419f049
Praveen240503 <96769580+Praveen240503@users.noreply.github.com>
2022-06-09 12:28:43 +0530

    def is_ugly(n):         if n==0:             return 0         for i in [2,3,5]:             while n%i==0:                 n/=i         return n==1 n=int(input()) if(is_ugly(n)):     print('Ugly Number') else:     print('Not Ugly Number')

---
+++ Ugly_number.py
@@ -0,0 +1,12 @@
+def is_ugly(n):
+        if n==0:
+            return 0
+        for i in [2,3,5]:
+            while n%i==0:
+                n/=i
+        return n==1
+n=int(input())
+if(is_ugly(n)):
+    print('Ugly Number')
+else:
+    print('Not Ugly Number')
