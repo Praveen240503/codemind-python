deea4239313083cc6f9b8ed828bcffde869117f0
Praveen240503 <96769580+Praveen240503@users.noreply.github.com>
2022-06-28 22:42:29 +0530

    x,y=map(int,input().split()) n=list(map(int,input().split())) c=0 for i in range(0,x):     if n[i]%y==0:         c+=1 print(c)

--- Count_elements_divisible_by_k.py
+++ Count_elements_divisible_by_k.py
@@ -1,7 +1,7 @@
 x,y=map(int,input().split())
-a=list(map(int,input().split()))
+n=list(map(int,input().split()))
 c=0
 for i in range(0,x):
-    if a[i]%y==0:
+    if n[i]%y==0:
         c+=1
 print(c)
