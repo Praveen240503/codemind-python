89a5ad342ecbb0ad8e11470460ac0414c9c68b09
Praveen240503 <96769580+Praveen240503@users.noreply.github.com>
2022-09-10 15:29:19 +0530

    n= int(input()) a = 64 for i in range(1,n+1):     for j in range(1, n+1-i):         print(' ', end='')     for j in range(1,i+1):         print('%c' %(a+j), end='')     for j in range(i-1,0,-1):         print('%c' %(a+j), end='')     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,9 +1,10 @@
-
-n=int(input())
+n= int(input())
+a = 64
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i==j or j==n-i+1:
-            print(n-i+1,end=" ")
-        else:
-            print(" ",end="")
+    for j in range(1, n+1-i):
+        print(' ', end='')
+    for j in range(1,i+1):
+        print('%c' %(a+j), end='')
+    for j in range(i-1,0,-1):
+        print('%c' %(a+j), end='')
     print()
